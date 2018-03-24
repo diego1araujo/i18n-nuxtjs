517e11e5ea64f6127f5ca225421490f9422fc495
Diego Araujo <diegoishigh@gmail.com>
2018-03-24 18:25:46 -0300

    Settings and .lock

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,29 @@
-export default {
-  loading: { color: 'cyan' },
-  router: {
-    middleware: 'i18n'
-  },
-  plugins: ['~/plugins/i18n.js'],
-  generate: {
-    routes: ['/', '/about', '/fr', '/fr/about']
-  }
-}
+module.exports = {
+    head: {
+        title: "",
+        meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            { hid: "description", name: "description", content: "Nuxt.js project" }
+        ],
+        link: [
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
+        ]
+    },
+    loading: { color: "cyan" },
+    router: {
+        middleware: "i18n"
+    },
+    plugins: ["~/plugins/i18n.js"],
+    generate: {
+        routes: [
+            "/",
+            "/about",
+            "/products",
+            "/pt",
+            "/pt/about",
+            "/pt/products",
+        ]
+    }
+};
